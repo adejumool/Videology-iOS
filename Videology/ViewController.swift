854bcffcd3e34204cdece307dc845905ddb34254
Tobi <adejumool@berea.edu>
2020-01-27 20:24:42 -0500

    Initial Commit

---
+++ Videology/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Videology
+//
+//  Created by Tobi Adejumo on 1/27/20.
+//  Copyright © 2020 Tobi Adejumo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
